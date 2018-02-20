5e608c43960d07dac24254262be8211e2c0f9026
Markoboby <mark.malaj@ynov.com>
2018-02-20 14:43:15 +0100

    Mise a jour de la page d'enregistrement

--- app/src/main/java/com/ingesup/docblayck/umtz/RegisterActivity.java
+++ app/src/main/java/com/ingesup/docblayck/umtz/RegisterActivity.java
@@ -1,7 +1,9 @@
 package com.ingesup.docblayck.umtz;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegisterActivity extends Activity {
 
@@ -9,5 +11,14 @@ public class RegisterActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_layout);
+    }
+
+    public void buttonAlreadyMemberClick(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void buttonSendFormRegister(View view){
+
     }
 }
