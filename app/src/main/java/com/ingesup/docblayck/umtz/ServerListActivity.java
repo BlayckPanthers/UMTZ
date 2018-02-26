0171e58647d43522b951da8ada56ad6e78e2f747
BlayckPanthers <fabienlebon974@gmail.com>
2018-02-26 13:12:26 +0100

    Ajout de la toolbar à partir de serverlistActivity. Action sur le bouton deconnexion : fait Ouvre un dialog proposant OUI/NON pour la deconnexion et redirige vers le main activity si oui
    Ajout item : About, download
    
    TODO : faire les actions de ceux-ci

--- app/src/main/java/com/ingesup/docblayck/umtz/ServerListActivity.java
+++ app/src/main/java/com/ingesup/docblayck/umtz/ServerListActivity.java
@@ -1,7 +1,14 @@
 package com.ingesup.docblayck.umtz;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -12,19 +19,20 @@ import com.ingesup.docblayck.umtz.Tasks.AsyncTaskServers;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ServerListActivity extends Activity {
+public class ServerListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.infrastructure_layout);
-
+        getSupportActionBar().show();
+        getSupportActionBar().setBackgroundDrawable(getDrawable(R.color.colorToolbar));
         // ListView affichant la liste des serveurs
         ListView mListView = (ListView) findViewById(R.id.serverList);
 
         // Liste contenant les serveurs générés.
         //List<Infrastructure> myInfras = genererInfra();
-        List<Infrastructure> myInfras;
+        /*List<Infrastructure> myInfras;
         try{
             myInfras = new AsyncTaskServers(this).execute("http://174.138.7.116:8080/CWS/api/centreon").get();
             InfrastructureAdapter adapter = new InfrastructureAdapter(ServerListActivity.this, myInfras);
@@ -33,7 +41,64 @@ public class ServerListActivity extends Activity {
             Toast.makeText(getApplicationContext(), GlobalData.getInstance().getUser().getEmail(),Toast.LENGTH_SHORT).show();
         }catch (Exception e){
 
+        } */
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        switch(id){
+            case R.id.action_logout :
+                //TODO : Dialog menant à la déconnexion
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage(R.string.dialog_message).setTitle(R.string.dialog_title);
+                builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        GlobalData.getInstance().setUser(null);
+                        Intent intent = new Intent(getApplicationContext(), LoginActivty.class);
+                        startActivity(intent);
+                    }
+                });
+                builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {}
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
+
+                break;
+            case R.id.action_about :
+                //TODO : Simple activity for showing the about section
+
+                Toast.makeText(this, "Action clicked 2 ", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.action_download :
+                //TODO : Download PDF
+
+
+                Toast.makeText(this, "Action clicked 3 ", Toast.LENGTH_LONG).show();
+                break;
+
+            default: return true;
+
         }
+
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -53,4 +118,7 @@ public class ServerListActivity extends Activity {
 
         return myList;
     }
+
+
+
 }
